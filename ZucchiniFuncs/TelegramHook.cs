02606ba2c85979ebc4ff638c516fcffec153c239
klemens_eisenstecken <klemens.eisenstecken@topcontrol.it>
2023-01-03 13:28:11 +0100

    Use UnixEpoch const from DateTime

--- ZucchiniFuncs/TelegramHook.cs
+++ ZucchiniFuncs/TelegramHook.cs
@@ -82,10 +82,7 @@ namespace ZucchiniFuncs
 
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
-            // Unix timestamp is seconds past epoch
-            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-            return dateTime;
+            return DateTime.UnixEpoch.AddSeconds(unixTimeStamp).ToLocalTime();
         }
     }
 }
