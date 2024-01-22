2b751cb7035899942be69567884c9db3bb41f288
Sebastian Zanoni <zanonisebastian@gmail.com>
2024-01-22 12:47:03 +0100

    Summarize month

--- ZucchiniFuncs/CommandHandler.cs
+++ ZucchiniFuncs/CommandHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Zucchetti;
+using Zucchini.Bot;
 
 namespace ZucchiniFuncs
 {
@@ -25,6 +27,7 @@ namespace ZucchiniFuncs
                     "/in" => HandleClockInAsync(),
                     "/out" => HandleClockOutAsync(),
                     "/stamps" => HandleRetrieveStampsAsync(),
+                    "/month" => HandleRetrieveWorkHoursOfMonth(command.Split(' ')[1..]),
                     _ => HandleUnknownCommandAsync(),
                 };
 
@@ -88,6 +91,125 @@ namespace ZucchiniFuncs
 
             result.AppendLine(String.Empty);
             result.AppendLine($"⏳ -- {difference.Hours}h {difference.Minutes}m");
+
+            return result.ToString();
+        }
+
+        private static IEnumerable<DateTime> AllDatesInMonth(int year, int month)
+        {
+            int days = DateTime.DaysInMonth(year, month);
+            for (int day = 1; day <= days; day++)
+            {
+                yield return new DateTime(year, month, day);
+            }
+        }
+
+        private static TimeSpan CalculateWorkingHours(IEnumerable<Stamp> stamps)
+        {
+            TimeSpan difference = new TimeSpan();
+            var pairs = stamps.Select((value, index) => new { value, index })
+                              .GroupBy(x => x.index / 2, x => x.value);
+            foreach (var pair in pairs)
+            {
+                var clockInStamp = pair.First();
+                var clockOutStamp = pair.Last();
+
+                difference += clockOutStamp.Time - clockInStamp.Time;
+            }
+
+            return difference;
+        }
+
+        private async Task<string> HandleRetrieveWorkHoursOfMonth(string[] parameters)
+        {
+            var result = new StringBuilder();
+            DateTime currentTime;
+            if (parameters.Length == 2)
+            {
+                currentTime = new DateTime(int.Parse(parameters[0]), int.Parse(parameters[1]), 1);
+            }
+            else
+            {
+                currentTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"));
+            }
+            SouthTyrolPublicHoliday southTyrolPublicHoliday = new();
+
+            var totalWorkingHours = TimeSpan.Zero;
+            var totalOvertime = TimeSpan.Zero;
+            var belowWorkTime = TimeSpan.Zero;
+            var giftedWorkTime = TimeSpan.Zero;
+
+            TimeSpan workingHourTarget = TimeSpan.FromHours(8);
+            TimeSpan overTimeStartTarget = workingHourTarget + TimeSpan.FromMinutes(30);
+
+            foreach (var day in AllDatesInMonth(currentTime.Year, currentTime.Month))
+            {
+                if (day > DateTime.Today)
+                {
+                    break;
+                }
+
+                var stamps = await client.RetrieveStampsAsync(DateOnly.FromDateTime(day));
+                stamps = stamps.OrderBy((stamp) => stamp.Time);
+
+                bool isWorkingDay = southTyrolPublicHoliday.IsWorkingDay(day);
+                TimeSpan workingHours = CalculateWorkingHours(stamps);
+                if (workingHours == TimeSpan.Zero && !isWorkingDay)
+                {
+                    continue;
+                }
+
+                var status = string.Empty;
+                if (day.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    var overtime = workingHours;
+                    overtime = TimeSpan.FromMinutes(15 * Math.Floor(overtime.TotalMinutes / 15));
+
+                    status = $" 🔺{overtime:hh\\:mm}";
+
+                    totalOvertime += overtime;
+                }
+                else if (workingHours > overTimeStartTarget)
+                {
+                    var overtime = workingHours - workingHourTarget;
+                    overtime = TimeSpan.FromMinutes(15 * Math.Floor(overtime.TotalMinutes / 15));
+
+                    status = $" 🔺{overtime:hh\\:mm}";
+
+                    totalOvertime += overtime;
+                }
+                else if (day == DateTime.Today)
+                {
+                    status = " 🕒";
+                }
+                else if (workingHours < workingHourTarget)
+                {
+                    var undertime = workingHourTarget - workingHours;
+                    status = $" 🔻{undertime:hh\\:mm}";
+
+                    belowWorkTime += undertime;
+                }
+                else
+                {
+                    giftedWorkTime += workingHours - workingHourTarget;
+                }
+
+                result.AppendLine($"<code>{day.ToString("ddd dd.MM")}</code>: {workingHours.ToString(@"hh\:mm")}{status}");
+            }
+
+            result.AppendLine();
+            result.AppendLine($"Overtime: {totalOvertime.ToString(@"hh\:mm")}");
+            result.AppendLine($"Below WorkTime: {belowWorkTime.ToString(@"hh\:mm")}");
+            result.AppendLine($"Gifted WorkTime: {giftedWorkTime.ToString(@"hh\:mm")}");
+
+            if (belowWorkTime > totalOvertime)
+            {
+                result.AppendLine($"Total 🔻 {(belowWorkTime - totalOvertime).ToString(@"hh\:mm")}");
+            }
+            else
+            {
+                result.AppendLine($"Total 🔺 {(totalOvertime - belowWorkTime).ToString(@"hh\:mm")}");
+            }
 
             return result.ToString();
         }
---
+++ ZucchiniFuncs/SouthTyrolPublicHoliday.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using PublicHoliday;
+
+namespace Zucchini.Bot;
+internal class SouthTyrolPublicHoliday : ItalyPublicHoliday
+{
+    //
+    // Summary:
+    //     Pentecost Monday
+    //
+    // Parameters:
+    //   year:
+    //     The year.
+    //
+    // Returns:
+    //     Date of in the given year.
+    private static DateTime PentecostMonday(int year)
+    {
+        return EasterMonday(year).AddDays(7 * 7);
+    }
+
+    public override IDictionary<DateTime, string> PublicHolidayNames(int year)
+    {
+        IDictionary<DateTime, string> holidayNames = base.PublicHolidayNames(year);
+        return holidayNames;
+    }
+
+    public override bool IsPublicHoliday(DateTime dt)
+    {
+        bool isPublicHoliday = base.IsPublicHoliday(dt);
+        if (!isPublicHoliday)
+        {
+            isPublicHoliday = dt == PentecostMonday(dt.Year);
+        }
+
+        return isPublicHoliday;
+    }
+}
